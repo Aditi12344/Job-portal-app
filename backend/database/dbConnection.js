052902997dd327661eb4cd5e28bbd51cc92f7831
Aditi Shukla <144916579+Aditi12344@users.noreply.github.com>
2025-07-08 19:49:49 +0530

    Update dbConnection.js

--- backend/database/dbConnection.js
+++ backend/database/dbConnection.js
@@ -1,17 +1,17 @@
-import mongoose from "mongoose"; //just mongoose import!
-import dotenv from "dotenv"
-dotenv.config()
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
-//Database connection here!
- const dbConnection  = ()=>{
-    mongoose.connect(process.env.DB_URL,{
-       dbName: "Job_Portal"
+const dbConnection = () => {
+  mongoose.connect(process.env.MONGO_URL, {
+    dbName: "Job_Portal",
+  })
+  .then(() => {
+    console.log("MongoDB Connected Successfully!");
+  })
+  .catch((error) => {
+    console.log(`Failed to connect: ${error}`);
+  });
+};
 
-    }).then(()=>{ //agar connect ho jaye toh!
-       console.log("MongoDB Connected Sucessfully !")
-    }).catch((error)=>{
-        console.log(`Failed to connect ${error}`) //warna error de do console me!
-    })
-    
-}
 export default dbConnection;
